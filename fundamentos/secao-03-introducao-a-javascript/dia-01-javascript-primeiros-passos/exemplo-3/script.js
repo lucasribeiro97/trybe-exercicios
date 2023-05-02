017b47ffe0db281dd881ad79ca964b9bb1108e2c
Lucas <lucasriibeiro23@gmail.com>
2023-05-02 15:35:10 -0300

    Fiz o exemplo 3 dos primeiros passos em js

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exemplo-3/script.js
@@ -0,0 +1,9 @@
+let nota = 70;
+
+if (nota >= 80) {
+    console.log("Parabéns!");
+} if (nota < 80 && nota >= 60) {
+    console.log("Voce está na lista de espera.")
+} if (nota < 60) {
+    console.log("Infelizmente, voce reprovou.");
+}
