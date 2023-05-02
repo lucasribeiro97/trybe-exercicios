4d10790022127a982838c0448c0733f91bd55684
Lucas <lucasriibeiro23@gmail.com>
2023-05-02 18:07:24 -0300

    fiz o exercicio 6 da secao 3 do dia 1.

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-06/script.js
@@ -0,0 +1,15 @@
+let peca = "bispo";
+
+switch (peca) {
+    case "cavalo":
+        console.log("L");
+        break;
+    case "bispo":
+        console.log("diagonais");
+        break;
+    case "torre":
+        console.log("verticais e horizontais");
+        break;
+    case "rainha":
+        console.log("Qualquer movimento");
+}
