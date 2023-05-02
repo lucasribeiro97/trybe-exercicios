df3d6e0abd4d7c07da979314667f45af17d74002
Lucas <lucasriibeiro23@gmail.com>
2023-05-02 15:46:07 -0300

    fiz o exemplo 4 dos primeiros passo em js

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exemplo-4/script.js
@@ -0,0 +1,7 @@
+let weekDay = "Sabado";
+ if (weekDay==="segunda-feira" || weekDay==="terca-feira" || weekDay==="quarta-feira" || weekDay==="quinta-feira" || weekDay==="sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe!");
+ } else {
+    console.log("FINALMENTE, descanso merecido! UwU!");
+ }
+
