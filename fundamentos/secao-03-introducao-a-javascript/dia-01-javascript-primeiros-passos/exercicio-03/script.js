b2310f217520bc1ec64863912544987ecb5bc818
Lucas <lucasriibeiro23@gmail.com>
2023-05-02 16:37:08 -0300

    Fiz o exercicios de 2 a 4 da secao 3 do dia 1

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-03/script.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 5;
+let c = 3;
+
+if (a>b && a>c) {
+    console.log(a);
+} if (b>a && b>c) {
+    console.log(b);
+} if (c>a && c>b) {
+    console.log(c);
+}
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-04/script.js
@@ -0,0 +1,9 @@
+let n = 0
+
+if (n < 0) {
+    console.log("negative");
+} if (n == 0) {
+    console.log("zero");
+} if (n > 0) {
+    console.log("positive")
+}
