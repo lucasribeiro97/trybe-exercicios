cee36b98ebc34b311ca2d0ce946adaaa39ee779a
Lucas <lucasriibeiro23@gmail.com>
2023-05-02 18:30:06 -0300

    fiz os exericicios de 7 a 9

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-07/script.js
@@ -0,0 +1,17 @@
+let nota = 400
+
+if (nota >= 90 && nota <= 100) {
+    console.log("A");
+} if (nota >= 80 & nota < 90) {
+    console.log("B");
+} if (nota >= 70 & nota < 80) {
+    console.log("C");
+} if (nota >= 60 & nota < 70) {
+    console.log("D");
+} if (nota >= 50 & nota < 60) {
+    console.log("E");
+} if (nota < 50 && nota > 0) {
+    console.log("F")
+} if (nota < 0 || nota > 100) {
+    console.log("[ERRO] Nota invalida!")
+}
