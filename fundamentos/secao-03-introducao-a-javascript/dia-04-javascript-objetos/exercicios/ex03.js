0d786e12643ee02095d1daeab5ef0550421da41e
Lucas <lucasriibeiro23@gmail.com>
2023-05-05 16:54:45 -0300

    comecei o exercicio 3 de objetos

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicios/ex03.js
@@ -0,0 +1,55 @@
+let order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        },
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        },
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      },
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  function customerInfo(order) {
+    // Adicione abaixo as informações necessárias.
+    let address = 'address'
+    let entregador = order.order.delivery.deliveryPerson;
+    let cliente = order.name;
+    let telefone = order.phoneNumber;
+    let rua = order[address].street;
+    let numero = order[address].number;
+    let apartamento = order[address].apartament;
+    console.log(`Olá, ${entregador}, entrega para: ${cliente}, Telefone: ${telefone}, R. ${rua}, Numero: ${numero}, AP: ${apartamento}`);
+  }
+  
+  customerInfo(order);
+  
+  function orderModifier(order) {
+    // Adicione abaixo as informações necessárias.
+  }
+  
+  orderModifier(order);
