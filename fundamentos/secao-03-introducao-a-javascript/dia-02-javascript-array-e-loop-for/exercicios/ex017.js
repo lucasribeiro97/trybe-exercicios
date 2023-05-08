0162fadc9764c3b383759ece47f63a4443984f33
Lucas <lucasriibeiro23@gmail.com>
2023-05-08 13:42:29 -0300

    fiz o exercicios bonus de loops e arrays

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios/ex017.js
@@ -0,0 +1,11 @@
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let index = 0; index < n; index += 1) {
+    inputLine = inputLine + symbol
+};
+
+for (lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    console.log(inputLine);
+};
