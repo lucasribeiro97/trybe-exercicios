f971e1d4683b948dec8a1df28f23d50501203f0d
Lucas <lucasriibeiro23@gmail.com>
2023-05-08 16:32:43 -0300

    fiz os exemplos das aulas da secao 3, dia 5

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exemplos/ex04.js
@@ -0,0 +1,11 @@
+let idade = 19;
+
+/* if (idade >= 18) {
+    console.log('Pode dirigir!')
+} else {
+    console.log('Nao pode dirigir!')
+}; */
+
+const verificaIdade = (idade >= 18) ? 'Pode dirigir' : 'Nao pode dirigir'
+
+console.log(verificaIdade)
