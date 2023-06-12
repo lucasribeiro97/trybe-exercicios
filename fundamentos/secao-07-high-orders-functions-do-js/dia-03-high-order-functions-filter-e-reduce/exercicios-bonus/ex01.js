aa21e494ec6e48dd060160d83705046bfced37b1
Lucas <lucasriibeiro23@gmail.com>
2023-06-12 15:37:03 -0300

    fiz os exercicios bonus de reduce

---
+++ fundamentos/secao-07-high-orders-functions-do-js/dia-03-high-order-functions-filter-e-reduce/exercicios-bonus/ex01.js
@@ -0,0 +1,12 @@
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+const flatten = () => {
+  return arrays.reduce((acc, curr) => {
+    return acc.concat(curr);
+  }, [])
+}
+console.log(flatten());
