c3eb656c15d44cceea44320c52a49b74c98a7b30
Lucas <lucasriibeiro23@gmail.com>
2023-06-13 16:28:44 -0300

    fiz exemplo de array destructuring

---
+++ fundamentos/secao-07-high-orders-functions-do-js/dia-05-array-e-default-destructuring/exemplos/ex01.js
@@ -0,0 +1,17 @@
+const primeNumbers = [17, 23, 37]
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+sum(primeNumbers[0], primeNumbers[2]) // 54
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [firstNumber, secondNumber, thirdNumber] = primeNumbers;
+
+const soma = (a, b) => {
+  console.log(a + b);
+}
+
+sum(firstNumber, thirdNumber);
