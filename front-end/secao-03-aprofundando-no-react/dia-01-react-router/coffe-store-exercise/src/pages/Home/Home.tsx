a24d2ddafb0b162c5cf6c8a2575854674899fa95
lucas <lucasriibeiro23@gmail.com>
2023-07-13 15:55:34 -0300

    navegando entre rotas

--- front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffe-store-exercise/src/pages/Home/Home.tsx
+++ front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffe-store-exercise/src/pages/Home/Home.tsx
@@ -1,6 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
   return (
-    <h1>Home page</h1>
+    <>
+      <h1>Coffeteria</h1>
+      <Link to="/coffes">
+        <button>Conheça nossos produtos</button>
+      </Link>
+    </>
   )
 }
 
