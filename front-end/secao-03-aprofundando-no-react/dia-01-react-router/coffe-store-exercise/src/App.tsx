0c807906db3f45b85ec84a76c676fd252732948e
lucas <lucasriibeiro23@gmail.com>
2023-07-14 16:47:19 -0300

    criei pagina de rota inexistente

--- front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffe-store-exercise/src/App.tsx
+++ front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffe-store-exercise/src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home';
 import CoffeeList from './pages/CoffeeList/CoffeeList';
 import Coffee from './pages/Coffee/Coffee';
 import NavBar from './components/NavBar/NavBar';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={ <Home /> } />
           <Route path='/coffees' element={ <CoffeeList /> } />
           <Route path='/coffees/:coffee' element={ <Coffee/> } />
+        <Route path='/*' element={ <NotFound /> } />
         </Routes>
     </>
   )
