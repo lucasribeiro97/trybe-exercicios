4d1e4f83566346df1d15dc197b87073d1de022f8
lucas <lucasriibeiro23@gmail.com>
2023-07-14 17:22:25 -0300

    ajustando arquivos

--- front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffe-store-exercise/src/App.tsx
+++ front-end/secao-03-aprofundando-no-react/dia-01-react-router/coffe-store-exercise/src/App.tsx
@@ -2,19 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import CoffeeList from './pages/CoffeeList/CoffeeList';
 import Coffee from './pages/Coffee/Coffee';
-import NavBar from './components/NavBar/NavBar';
 import NotFound from './pages/NotFound/NotFound';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <>
-      <NavBar />
-        <Routes>
-          <Route path='/' element={ <Home /> } />
-          <Route path='/coffees' element={ <CoffeeList /> } />
-          <Route path='/coffees/:coffee' element={ <Coffee/> } />
-        <Route path='/*' element={ <NotFound /> } />
-        </Routes>
+      <Routes>
+        <Route path="/" element={ <Layout /> }>
+          <Route index element={ <Home /> } />
+          <Route path="coffees" element={ <CoffeeList /> } />
+          <Route path="coffees/:coffee" element={ <Coffee /> } />
+        </Route>
+        <Route path="/*" element={ <NotFound /> } />
+      </Routes>
     </>
   )
 }
