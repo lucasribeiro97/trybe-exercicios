e1cf63473759c5063894c5162f31269ecf5e1e39
lucas <lucasriibeiro23@gmail.com>
2023-07-17 16:44:00 -0300

    incrementando a aplicacao

--- front-end/secao-03-aprofundando-no-react/dia-02-useEffect/space-station-exercise/src/App.tsx
+++ front-end/secao-03-aprofundando-no-react/dia-02-useEffect/space-station-exercise/src/App.tsx
@@ -17,6 +17,14 @@ function App() {
       setLoading(false);
     }
     fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
   }, []);
 
   if (loading) {
