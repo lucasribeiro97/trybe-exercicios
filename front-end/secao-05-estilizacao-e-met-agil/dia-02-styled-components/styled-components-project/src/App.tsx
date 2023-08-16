0875eaa75f98cea2e4f65857fab6c3ce88f38f75
lucas <lucasriibeiro23@gmail.com>
2023-08-16 16:10:08 -0300

    exercicio de styled components

---
+++ front-end/secao-05-estilizacao-e-met-agil/dia-02-styled-components/styled-components-project/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {Header, Container} from "./styles.tsx";
+
+function App() {
+  return (
+    <>
+      <Header>
+        <h1>Título</h1>
+      </Header>
+    
+      <Container>
+        <p>Conteúdo</p>
+      </Container>
+    </>
+  )
+}
+
+export default App
