1b3c634017df23aefc64a1672bcda9c5bb9e377a
lucas <lucasriibeiro23@gmail.com>
2023-11-09 15:51:36 -0300

    criei o token de validacao

--- back-end/secao-04-introducao-nodejs/dia-04-express-e-middleware/exercicio-middleware/src/utils/app.js
+++ back-end/secao-04-introducao-nodejs/dia-04-express-e-middleware/exercicio-middleware/src/utils/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const activities = require('./activities.json');
+const auth = require('../middlewares/auth');
 const validateName = require('../middlewares/validateName');
 const validatePrice = require('../middlewares/validatePrice');
 const validateDescription = require('../middlewares/validateDescription');
 const validateCreatedAt = require('../middlewares/validateCreatedAt');
 const validateRating = require('../middlewares/validateRating');
 const validateDifficulty = require('../middlewares/validateDifficulty');
+const generateToken = require('./generateToken');
 
 const app = express();
 app.use(express.json());
@@ -15,6 +17,7 @@ app.get('/activities', (_req, res) => {
 });
 
 app.post('/activities',
+  auth,
   validateName,
   validatePrice,
   validateDescription,
@@ -23,6 +26,17 @@ app.post('/activities',
   validateDifficulty,
   (req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso' });
+});
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+
+  if (!email || !password || !firstName || !phone) {
+    return res.status(401).json({ message: 'É necessário preencher todos os campos' });
+  }
+
+  const token = generateToken();
+  res.status(200).json({ token });
 });
 
 module.exports = app;
