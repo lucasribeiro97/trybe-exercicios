b9250439200a802b34052585e3469eedf430aaa9
lucasribeiro97 <lucasriibeiro23@gmail.com>
2024-01-24 14:53:30 -0300

    exercicio usando classe abstrata simulando super smash bross

---
+++ back-end/secao-09-POO-e-SOLID/dia-03-polimorfismo/exercicio-aula/Character.ts
@@ -0,0 +1,40 @@
+abstract class Character {
+  abstract talk(): void;
+  abstract specialMove(): void;
+}
+
+class MelleCharacter extends Character {
+  constructor(private _name: string, private _specialMoveName: string) {
+		super();
+  }
+
+	talk(): void {
+		console.log(`I'm ${this._name} and I'm a melle character.`);
+	}
+
+	specialMove(): void {
+		console.log(`${this._name} used special move: ${this._specialMoveName}!`);
+	}
+}
+
+class LongRangeCharacter extends Character {
+	constructor(private _name: string, private _specialMoveName: string) {
+		super();
+	}
+
+	talk(): void {
+		console.log(`I'm ${this._name} and I'm a long range character.`);
+	}
+
+	specialMove(): void {
+		console.log(`${this._name} used special move: ${this._specialMoveName}!`);
+	}
+}
+
+const yoshi = new MelleCharacter('Yoshi', 'Egg Throw');
+const samus = new LongRangeCharacter('Samus', 'Charge Shot');
+
+yoshi.talk();
+yoshi.specialMove();
+samus.talk();
+samus.specialMove();
